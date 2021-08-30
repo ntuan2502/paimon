3cc7482613fcec967e8cf22532dedfb8c46ea1ea
ntuan.2502 <ntuan.2502@gmail.com>
2021-08-30 19:51:49 +0700

    disabled click to unrelease characters

--- components/Characters/CharacterCard.js
+++ components/Characters/CharacterCard.js
@@ -3,7 +3,7 @@ import { star } from "../../lib/localData";
 
 export default function CharacterCard({ id, name, rarity, disabled }) {
   return (
-    <Link href={`/characters/${id}`}>
+    <Link href={`${disabled === false ? "/characters/" + id : "#"}`}>
       <a>
         <div className="relative bg-white py-6 px-6 rounded-3xl w-96 my-4 hover:shadow-xl cursor-pointer">
           <img
@@ -11,7 +11,9 @@ export default function CharacterCard({ id, name, rarity, disabled }) {
             src={`/img/character/card/character_${id}_card.webp`}
           />
           <div className="mt-8">
-            <p className="text-xl font-semibold my-2">{name}</p>
+            <p className="text-xl font-semibold my-2">
+              {name} {disabled === true ? "(*)" : ""}
+            </p>
             <div className="border-t-2"></div>
             <div className="flex justify-between">
               <div className="my-2">
