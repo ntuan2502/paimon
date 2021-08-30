3bbd74a15e416663d8106a7a91a1973860d44344
ntuan.2502 <ntuan.2502@gmail.com>
2021-08-30 17:50:21 +0700

    add download emojis button

--- pages/emojis/index.js
+++ pages/emojis/index.js
@@ -3,14 +3,24 @@ import path from "path";
 
 export default function EmojiPage({ posts }) {
   return (
-    <div className="flex flex-wrap justify-center">
-      {posts.map((post, index) => (
-        <img
-          src={`/img/emojis/${post.filename}`}
-          alt={post.filename}
-          key={index}
-        />
-      ))}
+    <div>
+      <div className="m-10 flex justify-center">
+        <a
+          href="/zip/emojis.zip"
+          class="flex-shrink-0 bg-red-600 text-white text-base font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+        >
+          Download ZIP File
+        </a>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        {posts.map((post, index) => (
+          <img
+            src={`/img/emojis/${post.filename}`}
+            alt={post.filename}
+            key={index}
+          />
+        ))}
+      </div>
     </div>
   );
 }
