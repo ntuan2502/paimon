6b99ff258cf5e6ccdf88ad975376aa4f9e3e3752
ntuan.2502 <ntuan.2502@gmail.com>
2021-09-05 06:38:09 +0700

    add aloy and kokomi images

--- pages/weapons/[name].js
+++ pages/weapons/[name].js
@@ -5,11 +5,41 @@ import WeaponAscension from "../../components/Weapons/WeaponAscension";
 import GenshinData from "genshin-data";
 import { getLocale } from "../../lib/localData";
 
-export default function WeaponPage({ weapon }) {
+export default function WeaponPage({ weapon, basePath }) {
   return (
     <div>
       <Head>
         <title>Genshin | {weapon.name}</title>
+        <meta itemProp="name" content={weapon.title} />
+        <meta itemProp="description" content={weapon.description} />
+        <meta
+          itemProp="image"
+          content={`${basePath}/img/weapons/weapon_${weapon.id}.webp`}
+        />
+
+        <meta itemProp="name" content={weapon.title} />
+        <meta itemProp="description" content={weapon.description} />
+        <meta
+          itemProp="image"
+          content={`${basePath}/img/weapons/weapon_${weapon.id}.webp`}
+        />
+
+        <meta property="og:url" content="https://paimon.vercel.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={weapon.title} />
+        <meta property="og:description" content={weapon.description} />
+        <meta
+          property="og:image"
+          content={`${basePath}/img/weapons/weapon_${weapon.id}.webp`}
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={weapon.title} />
+        <meta name="twitter:description" content={weapon.description} />
+        <meta
+          name="twitter:image"
+          content={`${basePath}/img/weapons/weapon_${weapon.id}.webp`}
+        />
       </Head>
       <div className="py-5">
         <div className="flex items-center justify-center">
@@ -51,9 +81,13 @@ export async function getStaticProps(context) {
   const weapons = await genshinData.weapons();
   const weapon = weapons.find((c) => c.id === context.params.name);
 
+  const basePath =
+    "https://raw.githubusercontent.com/ntuan2502/paimon/main/public";
+
   return {
     props: {
       weapon,
+      basePath,
     },
   };
 }
